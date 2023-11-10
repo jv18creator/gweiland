a6fc3767a4c334ed616ed703ec526939e50c7252
Jeet Viramgama <jviramgama5@gmail.com>
2023-11-11 00:24:29 +0200

    fix: removed dead code
    Signed-off-by: Jeet Viramgama <jviramgama5@gmail.com>

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,44 +6,19 @@ import 'package:gweiland/widgets/home/view/crypto_status_card.dart';
 import 'package:gweiland/widgets/home/view/filter_area.dart';
 import 'package:gweiland/widgets/home/view/search_input.dart';
 import 'package:gweiland/widgets/home/view/tab_area.dart';
-import 'package:gweiland/widgets/home/bloc/cryptocurrency_listings_bloc.dart';
-import 'package:bloc/bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SafeArea(
         bottom: false,
         child: Padding(
           padding: EdgeInsets.only(left: 20, top: 20, right: 20),
           child: Column(
             children: [
-              BlocBuilder<CryptocurrencyListingsBloc,
-                  CryptocurrencyListingsState>(
-                builder: (context, state) {
-                  print('state.data ${state.data}');
-                  return Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8.0),
-                    child: ElevatedButton.icon(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.tune,
-                      ),
-                      label: Text(
-                        "Apply Filters".toUpperCase(),
-                        style: const TextStyle(),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        textStyle: const TextStyle(fontSize: 16),
-                      ),
-                    ),
-                  );
-                },
-              ),
               Expanded(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
